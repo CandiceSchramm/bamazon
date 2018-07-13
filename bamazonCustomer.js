c8cc400620144c0682ae88409fa9a55b864bf22d
Candice <candice@Stuarts-MacBook.local>
2018-07-12 21:19:15 -0600

    displays intentory from db, prompts user to enter ID of item they'd like to select, and console logs that ID

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,28 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    console.log("Welcome to bamazon, where we deliver fresh micro-greens and sprouts right to your door! Here's what we have today!");
+    mainMenu()
+
   }) 
+  
+  function mainMenu() {
+    //query db to get product and ID for user
+    connection.query("SELECT product_name, item_id FROM products", function(err, results) {
+        if (err) throw err;
+        //displays results
+        for (var i = 0; i<results.length; i++){
+            console.log("ID: " + results[i].item_id + " - " + results[i].product_name );
+        }
+        inquirer
+        .prompt([
+            {
+                message: "What is the ID of the item that you'd like to purchase?",
+                type: "input",
+                name: "selectedId"
+            }
+        ]).then(function(answer){
+            console.log(answer.selectedId)
+        })
+    });
+};
